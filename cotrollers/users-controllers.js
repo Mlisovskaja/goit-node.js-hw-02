c73ba73daa6d80e6df6b01fca8e9032823e92304
Mari <mrs.tereshchenko@gmail.com>
2023-04-23 22:00:34 +0200

    avatars

--- cotrollers/users-controllers.js
+++ cotrollers/users-controllers.js
@@ -1,8 +1,13 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
+const fs = require("fs/promises");
+const path = require("path");
 
 const { User } = require('../models/user');
-const {SECRET_KEY} = process.env;
+const { SECRET_KEY } = process.env;
+
+const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
 const register = async(req, res, next)=> {
     try {
@@ -15,8 +20,10 @@ const register = async(req, res, next)=> {
         }
 
         const hashPassword = await bcrypt.hash(password, 10);
+    const avatarURL = gravatar.url(email);
     
-        const result = await User.create({ ...req.body, password: hashPassword });
+    const result = await User.create({...req.body, password: hashPassword, avatarURL});
+    
 
         res.status(201).json({
             email: result.email,
@@ -96,10 +103,28 @@ const logout = async (req, res, next) => {
     }
 }
 
+const updateAvatar = async(req, res, next)=> {
+    try {
+        const { _id } = req.user;
+        const { path: tempUpload, filename } = req.file;
+        const avatarName = `${_id}_${filename}`;
+        const resultUpload = path.join(avatarsDir, avatarName);
+        await fs.rename(tempUpload, resultUpload);
+        const avatarURL = path.join("avatars", avatarName);
+        await User.findByIdAndUpdate(_id, { avatarURL });
+
+        res.status(200).json({ avatarURL })
+    }
+    catch (error) {
+        next(error);
+    }
+}
+
 
 module.exports = {
     register,
     login,
     getCurrent,
-    logout
+    logout,
+    updateAvatar,
 }
